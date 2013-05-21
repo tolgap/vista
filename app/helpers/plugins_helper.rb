9661b6a03c68c9e7ed4d0abb339b4f0ebd161c85
tolgap <tolga@hoppinger.com>
2013-05-21 15:47:43 +0200

    show plugin info on plugin path

--- app/helpers/plugins_helper.rb
+++ app/helpers/plugins_helper.rb
@@ -3,11 +3,7 @@ module PluginsHelper
 	def create_list_item(plugin)
 		html = '<li class="' + plugin.status + ' plugin">'
 
-		if (plugin.updates === "available")
-			span = ' <span class="update label label-important">Has update</span>'
-		else
-			span = ' <span class="update label label-success">Up to date</span>'
-		end
+		span = has_update_span(plugin)
 
 		span += ' <span class="update label label-warning">Inactive</span>' if (plugin.status === "inactive")
 
@@ -15,6 +11,14 @@ module PluginsHelper
 
 		html += link_to plugin.name.titleize, plugin_path(plugin)
 		html += version + span + '</li>'
+	end
+
+	def has_update_span(plugin)
+		if (plugin.updates === "available")
+			span = ' <span class="update label label-important">Has update</span>'
+		else
+			span = ' <span class="update label label-success">Up to date</span>'
+		end
 	end
 
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -2,4 +2,13 @@ class Website < ActiveRecord::Base
   attr_accessible :name, :version, :server_id
   belongs_to :server
   has_many :plugins
+
+  # Class methods
+  class << self
+
+    def has_plugin(plugin)
+      matches = Plugin.where(:name => plugin.name).all
+    end
+
+  end
 end
