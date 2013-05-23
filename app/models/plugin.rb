3706247c188ce81c91ecca6aa967204145260a70
Tolga Paksoy <tolga@hoppinger.com>
2013-05-23 21:59:52 +0200

    show plugin information from Wordpress.org repository API

--- app/models/plugin.rb
+++ app/models/plugin.rb
@@ -1,4 +1,19 @@
 class Plugin < ActiveRecord::Base
   attr_accessible :name, :status, :updates, :version, :website_id
   belongs_to :website
+
+  # class methods
+  class << self
+
+  	def get_wp_plugin_info(plugin)
+  		require "net/http"
+  		require "php_serialize"
+
+  		uri = URI.parse("http://api.wordpress.org/plugins/info/1.0/" + plugin.name)
+  		res = Net::HTTP.get(uri)
+			
+			result = PHP.unserialize(res)
+  	end
+
+  end
 end
