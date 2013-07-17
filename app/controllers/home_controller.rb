641a143fd9c385d973ed9effc2ff559764011672
tolgap <tolga@hoppinger.com>
2013-07-17 23:23:11 +0200

    tidy up saving data and use Model.transaction for efficiency

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,48 +8,72 @@ class HomeController < ApplicationController
     wp_server = params[:server]
     data      = params[:data]
 
-    unless Server.exists?(:name => wp_server)
-      server = Server.new(name: wp_server)
-      server.save!
-    else
-      server = Server.find_by_name(wp_server)
-      server.touch
-      server.save!
-    end
+    server = save_server(wp_server)
 
-    data.each do |wp_website|
-      core_update = wp_website["has_update"] === "none"
-      unless Website.exists?(:name => wp_website["name"], :server_id => server.id)
-        website = Website.new(name: wp_website["name"], blog_name: wp_website["blog_name"], has_update: core_update, version: wp_website["version"], server_id: server.id, has_errors: wp_website["has_errors"])
-      else
-        website = Website.where(:name => wp_website["name"], :server_id => server.id).first
-        website.version    = wp_website["version"]
-        website.has_errors = wp_website["has_errors"]
-        website.has_update = core_update
-      end
-      
-      website.touch
-      website.save!
+    Website.transaction do
+      data.each do |wp_website|
+        website = save_website(wp_website, server)
 
-      next if wp_website["plugins"].nil?
+        next if wp_website["plugins"].nil?
 
-      wp_website["plugins"].each do |plugin|
-        plugin_update = plugin["update"] === "available"
-        unless Plugin.exists?(:name => plugin["name"], :website_id => website.id)
-          plugin = Plugin.new(name: plugin["name"], version: plugin["version"], has_update: plugin_update, status: plugin["status"], website_id: website.id)
-        else
-          plugin = Plugin.where(:name => plugin["name"], :website_id => website.id).first
-          plugin.version    = plugin["version"]
-          plugin.status     = plugin["status"]
-          plugin.has_update = plugin_update
+        Plugin.transaction do
+          wp_website["plugins"].each do |wp_plugin|
+            plugin = save_plugin(wp_plugin, website)
+          end
         end
-
-        plugin.touch
-        plugin.save!
+        
       end
     end
 
     redirect_to :action => "index"
   end
 
+  private
+
+  def save_server(wp_server)
+    unless Server.exists?(:name => wp_server)
+      server = Server.new(name: wp_server)
+    else
+      server = Server.find_by_name(wp_server)
+    end
+    server.touch
+    server.save!
+
+    server
+  end
+
+  def save_website(wp_website, server)
+    core_update = wp_website["has_update"] === "none"
+    unless Website.exists?(:name => wp_website["name"], :server_id => server.id)
+      website = Website.new(name: wp_website["name"], blog_name: wp_website["blog_name"], has_update: core_update, version: wp_website["version"], server_id: server.id, has_errors: wp_website["has_errors"])
+    else
+      website = Website.where(:name => wp_website["name"], :server_id => server.id).first
+      website.has_update     = core_update
+      website.version        = wp_website["version"]
+      website.has_errors     = wp_website["has_errors"]
+      website.website_errors = wp_website["website_errors"]
+    end
+    
+    website.touch
+    website.save!
+
+    website
+  end
+
+  def save_plugin(wp_plugin, website)
+    plugin_update = wp_plugin["update"] === "available"
+    unless Plugin.exists?(:name => wp_plugin["name"], :website_id => website.id)
+      plugin = Plugin.new(name: wp_plugin["name"], version: wp_plugin["version"], has_update: plugin_update, status: wp_plugin["status"], website_id: website.id)
+    else
+      plugin = Plugin.where(:name => wp_plugin["name"], :website_id => website.id).first
+      plugin.version    = wp_plugin["version"]
+      plugin.status     = wp_plugin["status"]
+      plugin.has_update = plugin_update
+    end
+
+    plugin.touch
+    plugin.save!
+
+    plugin
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130717115245) do
+ActiveRecord::Schema.define(:version => 20130717200439) do
 
   create_table "plugins", :force => true do |t|
     t.string   "name"
@@ -32,12 +32,13 @@ ActiveRecord::Schema.define(:version => 20130717115245) do
   create_table "websites", :force => true do |t|
     t.string   "name"
     t.string   "version"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.datetime "created_at",                        :null => false
+    t.datetime "updated_at",                        :null => false
     t.integer  "server_id"
     t.string   "blog_name"
     t.boolean  "has_update"
-    t.boolean  "has_errors", :default => false
+    t.boolean  "has_errors",     :default => false
+    t.text     "website_errors"
   end
 
 end
