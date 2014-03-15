e1c2cbac2f6e43aef62e0e62eacea6715f53111c
Tolga Paksoy <tlgpksy@gmail.com>
2014-03-15 23:15:40 +0100

    add website factory with 10 default plugins

--- spec/factories/website.rb
+++ spec/factories/website.rb
@@ -1,15 +1,21 @@
 FactoryGirl.define do
   factory :website do
+
     name { Faker::Internet.domain_word }
     version { "3.#{Faker::Number.number(1)}.#{Faker::Number.number(1)}" }
     blog_name { name.titleize }
     has_errors false
     has_update { [true, false].sample }
 
-    after(:create) do |website|
-      10.times do
-        website.plugins << create(:plugin, website: website)
+    factory :website_with_plugins do
+
+      after(:create) do |website|
+        10.times do
+          website.plugins << create(:plugin, website: website)
+        end
       end
+
     end
+
   end
 end
